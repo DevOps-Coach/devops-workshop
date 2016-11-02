53d3cd53e13e98139573f05cd17043526b0ac93c
martinliu <martin@aws-faq.com>
2016-11-03 01:33:34 +0800

    add code

---
+++ ms-lifecycle/scripts/bootstrap_ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Set ubuntu mirror"
+cp /vagrant/sources.list.trusty /etc/apt/sources.list
+
+echo "Installing Ansible..."
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y --force-yes ansible
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+
+#apt-get update -y
+#apt-get install -y python-pip python-dev
+#pip install ansible==1.9.2
+#mkdir -p /etc/ansible
+#touch /etc/ansible/hosts
+#cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+#mkdir -p /etc/ansible/callback_plugins/
+#cp /vagrant/ansible/plugins/human_log.py /etc/ansible/callback_plugins/human_log.py
